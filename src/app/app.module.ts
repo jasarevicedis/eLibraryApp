1599ad5f07bd6adf5c2d3e5e154016d5986c420a
jasarevicedis <e.jasarevic03@gmail.com>
2023-11-21 16:00:04 +0100

    added addition form components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,10 +6,16 @@ import { AppComponent } from './app.component';
 import { HeaderComponent } from './shared/header/header.component';
 import { SidebarComponent } from './shared/sidebar/sidebar.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { AddBookComponent } from './books/components/add-book/add-book.component';
+import { AddStudentComponent } from './students/components/add-student/add-student.component';
+import { AddLoanComponent } from './loans/components/add-loan/add-loan.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AddBookComponent,
+    AddStudentComponent,
+    AddLoanComponent
   ],
   imports: [
     BrowserModule,
