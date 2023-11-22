aa717a6954a8ffb1f74a383c9f377dfe69a48bac
jasarevicedis <e.jasarevic03@gmail.com>
2023-11-22 18:23:46 +0100

    book page basic layout updates

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,10 +4,12 @@ import { HomeComponent } from './home/home.component';
 import { BooksComponent } from './books/books.component';
 import { StudentsComponent } from './students/students.component';
 import { LoansComponent } from './loans/loans.component';
+import { BookPageComponent } from './books/components/book-page/book-page.component';
 
 const routes: Routes = [
   {path:'', component: HomeComponent},
   {path:'books', component: BooksComponent},
+  {path: 'books/1', component: BookPageComponent},
   {path: 'students', component: StudentsComponent},
   {path: 'loans', component: LoansComponent}
 ];
--- src/app/books/books.component.ts
+++ src/app/books/books.component.ts
@@ -1,20 +1,16 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { BookCardComponent } from './components/book-card/book-card.component';
-import { OpenBookService } from '../services/dialogs/open-book.service';
+import { RouterModule } from '@angular/router';
 
 
 @Component({
   selector: 'app-books',
   standalone: true,
-  imports: [CommonModule, BookCardComponent],
+  imports: [CommonModule, BookCardComponent, RouterModule],
   templateUrl: './books.component.html',
   styleUrls: ['./books.component.scss']
 })
 export class BooksComponent {
-  constructor(private openBookService: OpenBookService) {}
-
-  openBook(): void {
-    this.openBookService.openBookItemPage();
-  }
+  
 }
