a5ba7bbf4fc7b8accd91b39e0642109719968694
jasarevicedis <e.jasarevic03@gmail.com>
2023-11-23 09:56:32 +0100

    updated bookpage layout

--- src/app/books/books.component.ts
+++ src/app/books/books.component.ts
@@ -2,12 +2,19 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { BookCardComponent } from './components/book-card/book-card.component';
 import { RouterModule } from '@angular/router';
+import {MatInputModule} from '@angular/material/input';
+import {MatSelectModule} from '@angular/material/select';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatPaginatorModule} from '@angular/material/paginator';
 
 
 @Component({
   selector: 'app-books',
   standalone: true,
-  imports: [CommonModule, BookCardComponent, RouterModule],
+  imports: [
+    CommonModule, BookCardComponent, RouterModule,MatInputModule,
+    MatFormFieldModule,MatSelectModule,MatPaginatorModule
+  ],
   templateUrl: './books.component.html',
   styleUrls: ['./books.component.scss']
 })
