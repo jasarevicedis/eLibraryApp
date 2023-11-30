0c739f82c9ba6996ac17f788221fcbd399535cf2
jasarevicedis <e.jasarevic03@gmail.com>
2023-11-30 16:51:23 +0100

    added basic login layout

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -6,6 +6,7 @@ import { StudentsComponent } from './students/students.component';
 import { LoansComponent } from './loans/loans.component';
 import { BookPageComponent } from './books/components/book-page/book-page.component';
 import { StudentPageComponent } from './students/components/student-page/student-page.component';
+import { AuthComponent } from './auth/auth.component';
 
 const routes: Routes = [
   {path:'', component: HomeComponent},
@@ -13,7 +14,8 @@ const routes: Routes = [
   {path: 'books/1', component: BookPageComponent},
   {path: 'students', component: StudentsComponent},
   {path: 'students/1', component: StudentPageComponent},
-  {path: 'loans', component: LoansComponent}
+  {path: 'loans', component: LoansComponent},
+  {path: 'login', component: AuthComponent}
 ];
 
 @NgModule({
