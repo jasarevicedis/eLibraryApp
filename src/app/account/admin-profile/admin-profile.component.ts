ad46d50b09ffb9f88a42104093dee2c9526799ad
jasarevicedis <e.jasarevic03@gmail.com>
2023-12-06 22:04:30 +0100

    added pages for profile menu

---
+++ src/app/account/admin-profile/admin-profile.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-admin-profile',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './admin-profile.component.html',
+  styleUrls: ['./admin-profile.component.scss']
+})
+export class AdminProfileComponent {
+
+}
