17b164ff1155db9837c6704b79c0f89b803644b3
jasarevicedis <e.jasarevic03@gmail.com>
2023-12-07 20:48:56 +0100

    updated loan page layout

--- src/app/loans/loans.component.ts
+++ src/app/loans/loans.component.ts
@@ -1,13 +1,14 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import {MatPaginatorModule} from '@angular/material/paginator';
 
 @Component({
   selector: 'app-loans',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, MatPaginatorModule],
   templateUrl: './loans.component.html',
   styleUrls: ['./loans.component.scss']
 })
 export class LoansComponent {
-
+  itemsArray = new Array(20).fill(0);
 }
