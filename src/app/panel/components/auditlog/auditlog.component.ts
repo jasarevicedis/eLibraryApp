c4c3636e35275f3484b619213a10453ae1e04f8e
jasarevicedis <e.jasarevic03@gmail.com>
2023-12-07 18:55:56 +0100

    added audit log page

---
+++ src/app/panel/components/auditlog/auditlog.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-auditlog',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './auditlog.component.html',
+  styleUrls: ['./auditlog.component.scss']
+})
+export class AuditlogComponent {
+
+}
