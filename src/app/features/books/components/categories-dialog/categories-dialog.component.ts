b2f26eb501e57f1973e98b128c33f7bc04ba4812
jasarevicedis <e.jasarevic03@gmail.com>
2023-12-28 14:29:08 +0100

    add close dialog func to categories dialog

--- src/app/features/books/components/categories-dialog/categories-dialog.component.ts
+++ src/app/features/books/components/categories-dialog/categories-dialog.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { CategoriesService } from 'src/app/core/services/api/categories.service';
 import { Router } from '@angular/router';
+import { MatDialogRef } from '@angular/material/dialog';
 
 @Component({
   selector: 'app-categories-dialog',
@@ -12,11 +13,17 @@ import { Router } from '@angular/router';
 })
 export class CategoriesDialogComponent implements OnInit  {
   categoriesList: any;
-  constructor(private categoriesService: CategoriesService, private router: Router) {}
+  constructor(private categoriesService: CategoriesService, 
+    private router: Router,
+    private dialogRef: MatDialogRef<CategoriesDialogComponent>) {}
 
   ngOnInit(): void {
     this.categoriesService.getCategoriesData().subscribe((data) => {
       this.categoriesList = data;
     })
+  }
+
+  closeDialog(): void {
+    this.dialogRef.close();
   }
 }
